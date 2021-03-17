df0d03049748c3c68caa7d53ee9898e212355036
bradj <brad@bradjanke.com>
2021-03-17 11:08:01 -0500

    removing old

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,36 @@
-<script lang="ts">
-	export let segment: string;
+<script>
+  import { link } from 'svelte-spa-router';
+
 </script>
 
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/" use:link>
+      <h1 class="title is-1">Pocket</h1>
+    </a>
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+    </div>
 
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light" href="/login" use:link>
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </nav>
--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,32 +1,10 @@
-<script lang="ts">
-  import { goto,stores } from "@sapper/app";
-
-
-  const { session } = stores();
-  let username: string;
-  let password: string;
-
-	// export async function preload({ params }) {
-	// 	if (session && session.user) {
-	// 		return this.redirect(302, '/'); // TypeScript will know the type of `this` now
-	// 	}
-	// }
+<script>
+  import {login} from '$api';
+  let username;
+  let password;
 
   async function handleSubmit() {
-    const res = await fetch('/login', {
-      method: 'post',
-      body: JSON.stringify({username, password}),
-      headers: {'Content-Type': 'application/json'}
-    });
-
-    const parsed = await res.json();
-
-    console.log('parsed', parsed);
-
-    if (res) {
-      $session.user = parsed.user;
-      goto("/");
-    }
+    await login(username, password);
   }
 </script>
 
@@ -37,26 +15,28 @@
 	<title>Pocket - Login</title>
 </svelte:head>
 
-<div class="columns">
-  <div class="column is-3">
-    <h1 class="title">Pocket Login</h1>
-    <h2 class="subtitle">Enter your username and password</h2>
-    <div class="field">
-      <p class="control">
-        <input class="input" type="text" placeholder="Username" bind:value="{username}">
-      </p>
-    </div>
-    <div class="field">
-      <p class="control">
-        <input class="input" type="password" placeholder="Password" bind:value="{password}">
-      </p>
-    </div>
-    <div class="field">
-      <p class="control">
-        <button class="button is-success" on:click="{handleSubmit}">
-          Login
-        </button>
-      </p>
+<div class="container">
+  <div class="columns">
+    <div class="column is-3">
+      <h1 class="title">Login</h1>
+      <h2 class="subtitle">Enter your username and password</h2>
+      <div class="field">
+        <p class="control">
+          <input class="input" type="text" placeholder="Username" bind:value="{username}">
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <input class="input" type="password" placeholder="Password" bind:value="{password}">
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <button class="button is-success" on:click="{handleSubmit}">
+            Login
+          </button>
+        </p>
+      </div>
     </div>
   </div>
 </div>

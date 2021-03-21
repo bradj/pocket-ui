8eee0829f7571781197516e1b64f0e9a57b0749e
bradj <brad@bradjanke.com>
2021-03-21 15:11:05 -0500

    Adds typedef for feedItem. Feed is a bit more organized

--- src/components/FeedItem.svelte
+++ src/components/FeedItem.svelte
@@ -1,24 +1,50 @@
 <script>
   import { user } from '$store';
+  import { onMount } from 'svelte';
 
+  /**
+   * Incoming Feed Item
+   * @typedef {Object} FeedItem
+   * @type {object}
+   * @property {string} location - Media URL
+   * @property {string?} caption - Post caption
+   * @property {string} id - Post ID
+   * @property {Date} created_at - Post creation date
+   * @property {string} name - Page/User name
+   */
+
+  /**
+   * @type {FeedItem}
+  */
   export let feedItem;
+  
+  const formatDate = (date) => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString('en-US', options);
+  };
 </script>
 
 <style>
-  div.columns {
+  #feed-item-columns {
     border: 1px solid #d3d3d3;
   }
 </style>
 
-<div class="columns has-background-white mb-5 p-3 is-clearfix">
+<div class="columns has-background-white mb-5 p-3 is-clearfix" id="feed-item-columns">
   <div class="column is-three-quarters">
     <figure class="image">
       <img src="{`http://localhost:8080/api${feedItem.location}`}" style="max-height: 620px;" alt="" height="620px">
     </figure>
-    <p>Caption: {feedItem.caption}</p>
-    <p>Date: {feedItem.created_at}</p>
+    <div class="columns">
+      <div class="column is-half">
+        <p>{feedItem.name}</p>
+      </div>
+      <div class="column is-half has-text-right">
+        {formatDate(feedItem.created_at)}
+      </div>
+    </div>
+    <p>{feedItem.caption}</p>
   </div>
   <div class="column">
-    Second column blah?
   </div>
 </div>
